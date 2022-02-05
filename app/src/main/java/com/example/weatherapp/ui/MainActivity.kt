757f283fc5e8949be46add52bb466c65e95c076f
robsondmf1234@gmail.com <Itupeva123@>
2022-02-05 19:30:22 -0300

    Added imports and create navigation fragments

---
+++ app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.weatherapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+
+        binding.bottomNav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/ui/weather/future/list/FutureListWeatherFragment.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp.ui.weather.future.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weatherapp.R
+
+class FutureListWeatherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_future_list_weather, container, false)
+    }
+
+}
