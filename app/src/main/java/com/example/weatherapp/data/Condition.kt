1a294f2a2f3fc1c6c02ec919d923fe9172e1c32d
robsondmf1234@gmail.com <Itupeva123@>
2022-11-02 10:44:31 -0300

    feat - Save for Backup.

---
+++ app/src/main/java/com/example/weatherapp/data/Condition.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.data
+
+data class Condition(
+    val text: String,
+    val icon: String,
+    val code: Int
+)
---
+++ app/src/main/java/com/example/weatherapp/data/CurrentWeatherResponse.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapp.data
+
+import com.google.gson.annotations.SerializedName
+
+
+data class CurrentWeatherResponse(
+    @SerializedName("current")
+    val currentWeatherEntry: CurrentWeatherEntry,
+    val location: Location
+)
